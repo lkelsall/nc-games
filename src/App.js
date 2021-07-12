8fe70aa92fc2702a644456cfaab21ee3bbe21090
lkelsall <kelsall.lewis@gmail.com>
2021-07-12 14:46:32 +0100

    Implement User conext and display user in Header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import SingleReview from "./components/SingleReview";
 import Reviews from "./components/Reviews";
+import { UserContext } from "./contexts/user";
 
 function App() {
+  const [user, setUser] = useState("cooljmessy");
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Switch>
-        <Route path="/reviews/id/:review_id">
-          <SingleReview />
-        </Route>
-        <Route path="/reviews/category/:category_slug">
-          <Reviews />
-        </Route>
-        <Route path="/">
-          <Reviews />
-        </Route>
-      </Switch>
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="App">
+        <Header />
+        <Nav />
+        <Switch>
+          <Route path="/reviews/id/:review_id">
+            <SingleReview />
+          </Route>
+          <Route path="/reviews/category/:category_slug">
+            <Reviews />
+          </Route>
+          <Route path="/">
+            <Reviews />
+          </Route>
+        </Switch>
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../contexts/user";
 
 const Header = () => {
+  const { user } = useContext(UserContext);
   return (
     <div className="Header">
-      <Link to="/">NC Games</Link>
+      <Link to="/">
+        NC Games <span id="username">{user}</span>
+      </Link>
     </div>
   );
 };
