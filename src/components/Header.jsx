69693308d44d14a9542f4e0db1cce768a14cb00c
lkelsall <kelsall.lewis@gmail.com>
2021-07-12 12:21:10 +0100

    Create Header, Nav and Reviews components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>NC Games</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,13 @@
+const Reviews = () => {
+  return (
+    <div>
+      <ul>
+        <li>Review</li>
+        <li>Review</li>
+        <li>Review</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Reviews;
